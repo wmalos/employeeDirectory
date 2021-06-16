9dc9122fea8c937a8ea0068cff67cf9db6f4c3d3
Will <wmalos22@gmail.com>
2021-06-15 17:41:32 -0700

    edited header

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,9 @@ function Header() {
   return (
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
-        <h1 class="display-4">Fluid jumbotron</h1>
+        <h1 class="display-4">Employee Directory</h1>
         <p class="lead">
-          This is a modified jumbotron that occupies the entire horizontal space
-          of its parent.
+          Search for employees by first or last name.
         </p>
       </div>
     </div>
